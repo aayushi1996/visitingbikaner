e3f28ff139e25119bc1c6fb27fb3f72391cef0ac
aayushi <aayushi.dassani@sunarctechnologies.com>
2018-02-01 17:25:38 +0530

    finally tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
+import {Observable} from 'rxjs/Observable';
+// import 'rxjs/Rx';
 
 
 @Component({
@@ -8,7 +10,39 @@ import * as firebase from 'firebase';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  // isConnected: Observable<boolean>;
+  online = window.navigator.onLine;
+
+  constructor() {
+    // if (navigator.onLine) {
+    //   alert('online');
+    // } else {
+    //   alert('offline');
+    // }
+
+    // this.isConnected = Observable.merge(
+    //   Observable.of(navigator.onLine),
+    //   Observable.fromEvent(window, 'online').map(() => true),
+    //   Observable.fromEvent(window, 'offline').map(() => false));
+
+    window.addEventListener('load', function() {
+      const status = document.getElementById('status');
+      const log = document.getElementById('log');
+
+      function updateOnlineStatus(event) {
+        const condition = navigator.onLine ? 'online' : 'offline';
+
+        status.className = condition;
+        status.innerHTML = condition.toUpperCase();
+
+
+      }
+
+      // // window.addEventListener('online',  updateOnlineStatus);
+      // window.addEventListener('offline', updateOnlineStatus);
+    });
+
+  }
   ngOnInit() {
     firebase.initializeApp({
       apiKey: 'AIzaSyAmPj-j0neByBI4Q6HpBWVZlypBjRbN-NA',
